9fc87f36453d8bfa8b174546f2b903d29685d725
Daniel Armstrong <dany.armgstrong90@gmail.com>
2022-05-06 05:19:25 -0700

    minting completed.

--- programs/sol-mint-nft/src/lib.rs
+++ programs/sol-mint-nft/src/lib.rs
@@ -1,15 +1,108 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program::invoke;
+use anchor_spl::token::mint_to;
+use anchor_spl::token::{MintTo, Token};
+use mpl_token_metadata::instruction::{create_metadata_accounts_v2};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9FKLho9AUYScrrKgJbG1mExt5nSgEfk1CNEbR8qBwKTZ");
 
 #[program]
 pub mod sol_mint_nft {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn mint_nft(
+        ctx: Context<MintNFT>,
+        creator_key: Pubkey,
+        uri: String,
+        title: String
+    ) -> Result<()> {
+        msg!("Nft token minting:");
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        mint_to(cpi_ctx, 1);
+        msg!("Token minted !!!");
+
+        msg!("Metadata account creating:");
+        let accounts = vec![
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        let creators = vec![
+            mpl_token_metadata::state::Creator {
+                address: creator_key,
+                verified: false,
+                share: 100,
+            },
+            mpl_token_metadata::state::Creator {
+                address: ctx.accounts.mint_authority.key(),
+                verified: false,
+                share: 0,
+            },
+        ];
+        invoke(
+            &create_metadata_accounts_v2(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.payer.key(),
+                title,
+                std::string::ToString::to_string("symb"),
+                uri,
+                Some(creators),
+                1,
+                true,
+                false,
+                None,
+                None,
+            ),
+            &accounts
+        );
+        msg!("Metadata account created !!!");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MintNFT<'info> {
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub mint: UncheckedAccount<'info>,
+
+    pub token_program: Program<'info, Token>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_metadata_program: UncheckedAccount<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub payer: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub rent: AccountInfo<'info>,
+}
--- tests/sol-mint-nft.ts
+++ tests/sol-mint-nft.ts
@@ -2,15 +2,147 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolMintNft } from "../target/types/sol_mint_nft";
 
-describe("sol-mint-nft", () => {
+import {
+  TOKEN_PROGRAM_ID,
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAddress,
+  createInitializeMintInstruction,
+  MINT_SIZE,
+} from "@solana/spl-token";
+
+const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
+
+const nftName = "Animal(97): Elephant";
+const nftDescription = "This is animal nft of daniel's elephant.";
+const nftImageUrl = "https://bafybeihshos2fqh5nlz27j26fyvfy3ctwd2t3rgjoslsmmzxzykxhzwbea.ipfs.infura-ipfs.io";
+
+async function ipfs_metadata_upload() {
+  const ipfsClient = require("ipfs-http-client");
+  const ipfs = ipfsClient.create({
+    host: "ipfs.infura.io",
+    port: 5001,
+    protocol: "https",
+  });
+
+  const nftMetadata = {
+    name: nftName,
+    symbol: "ani-nft",
+    description: nftDescription,
+    image: nftImageUrl,
+    attributes: [
+      {
+        trait_type: "size",
+        value: "very big"
+      },
+      {
+        trait_type: "live in",
+        value: "land"
+      },
+      {
+        trait_type: "food",
+        value: "glass"
+      }
+    ]
+  };
+  const nftMetadataIpfs = await ipfs.add(JSON.stringify(nftMetadata));
+  if (nftMetadataIpfs == null) {
+    return '';
+  } else {
+    return nftMetadataIpfs.path;
+  }
+};
+
+async function sol_mint_nft() {
+  var metadataUri = await ipfs_metadata_upload();
+  if (metadataUri == '') {
+    console.log("IPFS metadata upload failed!");
+    return;
+  }
+  metadataUri = `https://ipfs.infura.io/ipfs/${metadataUri}`;
+  console.log("IPFS metadata uri: ", metadataUri);
+
+// describe("sol-mint-nft", async () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolMintNft as Program<SolMintNft>;
+  console.log("Program Id: ", program.programId.toBase58());
+  console.log('Mint Size: ', MINT_SIZE);
+  const lamports = await program.provider.connection.getMinimumBalanceForRentExemption(MINT_SIZE);
+  console.log("Mint Account Lamports: ", lamports);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  const getMetadata = async (mint: anchor.web3.PublicKey): Promise<anchor.web3.PublicKey> => {
+    return (await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from("metadata"),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mint.toBuffer(),
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    ))[0];
+  };
+
+  const mintKey: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+
+  const nftTokenAccount = await getAssociatedTokenAddress(
+    mintKey.publicKey,
+    provider.wallet.publicKey
+  );
+  console.log("NFT Account: ", nftTokenAccount.toBase58());
+
+  const mint_tx = new anchor.web3.Transaction().add(
+    anchor.web3.SystemProgram.createAccount({
+      fromPubkey: provider.wallet.publicKey,
+      newAccountPubkey: mintKey.publicKey,
+      space: MINT_SIZE,
+      programId: TOKEN_PROGRAM_ID,
+      lamports,
+    }),
+    createInitializeMintInstruction(
+      mintKey.publicKey,
+      0,
+      provider.wallet.publicKey,
+      provider.wallet.publicKey,
+    ),
+    createAssociatedTokenAccountInstruction(
+      provider.wallet.publicKey,
+      nftTokenAccount,
+      provider.wallet.publicKey,
+      mintKey.publicKey
+    )
+  );
+  const res = await program.provider.sendAndConfirm(mint_tx, [mintKey]);
+  console.log("Mint key: ", mintKey.publicKey.toString());
+  console.log("User: ", provider.wallet.publicKey.toString());
+
+  const metadataAddress = await getMetadata(mintKey.publicKey);
+  console.log("Metadata address: ", metadataAddress.toBase58());
+
+  // it("Is initialized!", async () => {
+    const tx = await program.rpc.mintNft(
+      mintKey.publicKey,
+      metadataUri,
+      nftName,
+      {
+        accounts: {
+          mintAuthority: provider.wallet.publicKey,
+          mint: mintKey.publicKey,
+          tokenAccount: nftTokenAccount,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          metadata: metadataAddress,
+          tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+          payer: provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        }
+      }
+    );
     console.log("Your transaction signature", tx);
-  });
-});
+  // });
+// });
+};
+
+sol_mint_nft();
